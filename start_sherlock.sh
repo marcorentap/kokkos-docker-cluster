71f7de93c169f90b6dc02c685294ab64822fe1d5
Marc Owen <marcowen20@gmail.com>
2024-02-17 21:34:19 +0900

    changed sherlock hostname

--- start_sherlock.sh
+++ start_sherlock.sh
@@ -1,5 +1,5 @@
-docker container rm sherlock-$(hostname)
-docker run -it --name sherlock-$(hostname) --hostname sherlock-$(hostname) -u compute \
+docker container rm kokkos-sherlock-$(hostname)
+docker run -it --name kokkos-sherlock-$(hostname) --hostname kokkos-sherlock-$(hostname) -u compute \
 --mount type=bind,source=./shared,destination=/shared \
 --network kokkos-overlay kokkos-sherlock \
 bash
