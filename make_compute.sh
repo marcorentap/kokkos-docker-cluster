1dde13b707ef0edcf9b4be8f011f28bedd4468c0
Marc Owen <marcowen20@gmail.com>
2024-02-20 20:03:00 +0900

    Separate ssh key generation

--- make_compute.sh
+++ make_compute.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-mkdir -p ssh
-rm ssh/id_rsa ssh/id_rsa.pub
-ssh-keygen -f ssh/id_rsa -N ''
-
 hpccm --recipe recipes/compute-recipe.py --format docker > Dockerfile_compute
 docker build -t kokkos-compute -f Dockerfile_compute .
